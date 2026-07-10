469c5bf4828e9e981db3d4796b0c2c7532d127df
Jared Adam <jared.adam@student.montana.edu>
2026-07-09 21:28:20 -0600

    pdf plot
    WUE plot sent

--- Scripts/2026/WUE.r
+++ Scripts/2026/WUE.r
@@ -6,14 +6,13 @@ library(tidyverse)
 # data ####
 
 wue <- X2026_7_8_WUE_pilot
+mos <- X2026_7_8_WUE_pilot_moisture
 
-
-# view ####
+# wue ####
 
 
 # bar to psi is 1 bar = 14.504 psi
-
-wue %>% 
+p1 <- wue %>% 
   mutate(trt = as.factor(trt),
          plant = as.factor(plant), 
          bar = as.numeric(bar), 
@@ -22,8 +21,13 @@ wue %>%
   mutate(day = as.factor(case_when(date == "2026-06-30" ~ 1,
                          date == '2026-07-01' ~ 2,
                          date == "2026-07-02" ~ 3))) %>% 
+mutate(trt = as.factor(case_when(trt == "w" ~ "Control",
+                         trt == "b" ~ "Hole punch",
+                         trt == "r" ~ "Leaf tip"))) %>% 
+  mutate(psi = bar*14.504) %>% 
   na.omit() %>% 
-  ggplot(aes(x = time, y = bar, color = trt)) +
+  print(n = Inf) %>% 
+  ggplot(aes(x = time, y = psi, color = trt)) +
     geom_smooth(method = 'gam',
                 formula = y ~ s(x, k=4))+
     facet_grid(vars(date))+
@@ -37,6 +41,28 @@ wue %>%
         legend.title = element_text(size = 20))+
   guides(color=guide_legend(title="Damage Type"))+
   scale_color_brewer(palette = "Dark2")+
-  labs(x = "Elevation",
-       y = "Damage")
+  labs(x = "Time of day (24hr)",
+       y = "PSI")+
+  xlim(250,450)
+
+
+ggsave('WUE_Pilot.pdf',
+       p1,
+       width = 6,
+       height = 6,
+       units='in',
+       dpi=600)
+
+# moisture ####
+
+mos %>% 
+  select(1:8) %>% 
+  mutate(trt = as.factor(case_when(trt == 'w' ~ 'Control',
+                         trt == 'b' ~ 'Hole punch',
+                         trt == 'r' ~ 'Leaf tip'))) %>%
+  ggplot(aes(x = tag, y = water, color = trt))+
+  geom_smooth(method = 'gam', 
+              formula = y ~ s(x, k=4))
+  
+
 
