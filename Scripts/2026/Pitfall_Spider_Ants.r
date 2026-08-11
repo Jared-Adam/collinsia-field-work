3d91818e0c47ea6935efbdc2f92eb3e4a75622ec
Jared Adam <jared.adam@student.montana.edu>
2026-08-11 10:16:03 -0600

    cleaning
    leaving to go work somewhere else

--- Scripts/2026/Pitfall_Spider_Ants.r
+++ Scripts/2026/Pitfall_Spider_Ants.r
@@ -11,7 +11,26 @@ q <- X2026_7_22_QuadratIDs
 
 q
 
-pf <- pf %>% 
+pf <- pf %>% # match q name
   rename(qtag = Quadrat)
 
-inner_join(q, pf, by = "qtag") %>% 
+df <- inner_join(q, pf, by = "qtag") 
+unique(df$loc)
+
+df %>% 
+  mutate(el = case_when(band == '1' & loc == 'MC' ~ '5400',
+                        band == '2'& loc == 'MC' ~'6150',
+                        band == '3' & loc == 'MC'~ '6320',
+                        band == '4' & loc == 'MC'~ '6760', 
+                        band == '5' & loc == 'MC'~ '7200',
+                        band == '6' & loc == 'MC'~ '7900', 
+                        band == '7' & loc == 'MC'~ '8200',
+                        band == '8'& loc == 'MC' ~ '8370',
+                        band == '9' & loc == 'MC'~ '8800',
+                        band == '10'& loc == 'MC' ~ '9150')) %>%
+  mutate(el = as.numeric(el)) %>% 
+  mutate(el = el*0.3) %>% 
+  relocate(Date, el)
+  ggplot(aes(x = band, y = Spiders))+
+  geom_smooth(method = 'gam',
+              formula = )
