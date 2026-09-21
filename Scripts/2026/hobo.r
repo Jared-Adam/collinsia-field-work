75d9b8ba4834715d7ff53e997b947faa2e1148b9
jared-adam <jared.adam@student.montana.edu>
2026-09-21 10:45:08 -0600

    hobo cleaning
    column updates

---
+++ Scripts/2026/hobo.r
@@ -0,0 +1,31 @@
+# packages ####
+library(tidyverse)
+library(readr)
+
+# data ####
+
+# step 1: change 1:1 to "q" on all .csv files
+# step 2: merge
+
+path <- list.files(path = "Data/2026/1csv", pattern = "*.csv", full.names = TRUE)
+
+test <-  path %>% 
+  map(~ read_csv(.x) %>% 
+  rename(Q = 1)) %>% 
+  reduce(full_join, by = "Q") %>% 
+  print(n = 10)
+
+cols <- test %>% 
+  select(1:4) %>% 
+  rename(qtag = Q,
+  number = "#.x",
+  temp_f = "Temp, °F (LGR S/N: 10567939, SEN S/N: 10567939, LBL: 1B3G)"
+  ) %>% 
+  separate_wider_delim(
+    cols = 'Date Time, GMT-06:00.x',
+    delim = " ",
+    names = c("date", "time")
+  )
+
+unique(cols$qtag)
+
